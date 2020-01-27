4e25ac580bbd4287ec5f7d8c512b1381b985fa93
vtumanov782 <vtumanov782@gmail.com>
2020-01-27 14:46:55 +0300

    i18n reviews

--- client/components/Reviewes.tsx
+++ client/components/Reviewes.tsx
@@ -1,52 +1,61 @@
 import React from "react";
 import ReviewItem from "./ReviewItem";
+import { WithTranslation } from "next-i18next";
+import { withTranslation } from "../../lib/i18n";
 
-export default class Reviews extends React.Component {
-  render() {
-    return (
-      <section className="bg-light page-section" id="reviews">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12 text-center">
-              <h2 className="section-heading text-uppercase">Отзывы о нас</h2>
-              <h3 className="section-subheading text-muted">
-                Мы всегда рады получить от вас обратную связь.
-              </h3>
+interface IReviewsProps extends WithTranslation {}
+
+export default withTranslation()(
+  class Reviews extends React.Component<IReviewsProps> {
+    render() {
+      const { t } = this.props;
+
+      return (
+        <section className="bg-light page-section" id="reviews">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12 text-center">
+                <h2 className="section-heading text-uppercase">
+                  {t("reviews.heading")}
+                </h2>
+                <h3 className="section-subheading text-muted">
+                  {t("reviews.subheading")}
+                </h3>
+              </div>
+            </div>
+            <div className="row">
+              <ReviewItem
+                imageUrl="img/team/1.jpg"
+                name={t("reviews.1.name")}
+                project={t("reviews.1.project")}
+                projectUrl="#PortfolioItem01"
+                message={t("reviews.1.message")}
+              />
+              <ReviewItem
+                imageUrl="img/team/2.jpg"
+                name={t("reviews.2.name")}
+                project={t("reviews.2.project")}
+                projectUrl="#PortfolioItem06"
+                message={t("reviews.2.message")}
+              />
+              <ReviewItem
+                imageUrl="img/team/3.jpg"
+                name={t("reviews.3.name")}
+                project={t("reviews.3.project")}
+                projectUrl="#PortfolioItem04"
+                message={t("reviews.3.message")}
+              />
+            </div>
+            <div className="row">
+              <div className="col-lg-8 mx-auto text-center">
+                <p className="large text-muted">
+                {t("reviews.outro")}
+                </p>
+              </div>
             </div>
           </div>
-          <div className="row">
-            <ReviewItem
-              imageUrl="img/team/1.jpg"
-              name="Александра"
-              project="Офис. Москва, Малая Пролетарская"
-              projectUrl="#PortfolioItem01"
-              message="Всё очень понравилось. Ремонт офиса выполнили быстро и качественно. Спасибо, Интериор!"
-            />
-            <ReviewItem
-              imageUrl="img/team/2.jpg"
-              name="Михаил"
-              project="Детская комната"
-              projectUrl="#PortfolioItem06"
-              message="Возникла необходимость сделать детскую комнату вместо бывшей гостинной. Все получилось хорошо и красиво, ребенок доволен и мы тоже:)"
-            />
-            <ReviewItem
-              imageUrl="img/team/3.jpg"
-              name="Елена"
-              project="Кафе. Москва, Громова 17"
-              projectUrl="#PortfolioItem04"
-              message="Компания Интериор занималась обустройством помещения для гостей в нашем кафе. Также они помогли нам с планировкой помещения, подбором материалов и цветов, за что им огромное спасибо!"
-            />
-          </div>
-          <div className="row">
-            <div className="col-lg-8 mx-auto text-center">
-              <p className="large text-muted">
-                Мы учитываем все ваши пожелания, чтобы развиваться и становиться
-                лучше. Мы рады любым вашим отзывам!
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
-    );
+        </section>
+      );
+    }
   }
-}
+);
