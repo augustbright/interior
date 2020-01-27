0f3e4e6534ccbca43600bb50ab33ec2b50d60fd7
vtumanov782 <vtumanov782@gmail.com>
2020-01-27 14:33:30 +0300

    benefits, portfolio

--- client/components/Benefits.tsx
+++ client/components/Benefits.tsx
@@ -1,84 +1,96 @@
 import React from "react";
 import BenefitThumbnail from "./BenefitThumbnail";
 import CallMeButton from "./CallMeButton";
+import { WithTranslation } from "next-i18next";
+import { withTranslation } from "../../lib/i18n";
 
-export default class Benefits extends React.Component {
-  render() {
-    return (
-      <section className="bg-light page-section" id="benefits">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12 text-center">
-              <h2 className="section-heading text-uppercase">
-                Почему нас выбирают?
-              </h2>
-              <h3 className="section-subheading text-muted">
-                Вот лишь некоторые из наши преимуществ...
-              </h3>
+interface IBenefitsProps extends WithTranslation {}
+
+export default withTranslation()(
+  class Benefits extends React.Component<IBenefitsProps> {
+    render() {
+      const { t } = this.props;
+
+      return (
+        <section className="bg-light page-section" id="benefits">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12 text-center">
+                <h2 className="section-heading text-uppercase">
+                  {t("benefits.heading")}
+                </h2>
+                <h3 className="section-subheading text-muted">
+                  {t("benefits.subheading")}
+                </h3>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-lg-12">
+                <ul className="timeline">
+                  <BenefitThumbnail
+                    heading={t("benefits.1.heading")}
+                    subheading={t("benefits.1.subheading")}
+                    image="./img/about/1.jpg"
+                    detailHeading={t("benefits.1.detailsHeading")}
+                    detailURL="#portfolio"
+                  >
+                    {t("benefits.1.text.interior is a leading repairs company")}{" "}
+                    <strong>{t("benefits.1.text.more than 10 years")}</strong>
+                    {t("benefits.1.text.of experience")}{" "}
+                    <strong>{t("benefits.1.text.more than 300 orders")}</strong>
+                    .{" "}
+                    {t(
+                      "benefits.1.text.we know everything about repairs and we love it"
+                    )}
+                    .
+                  </BenefitThumbnail>
+                  <BenefitThumbnail
+                    inverted
+                    heading={t("benefits.2.heading")}
+                    subheading={t("benefits.2.subheading")}
+                    image="./img/about/2.jpg"
+                  >
+                    {t("benefits.2.text.our clients can expect")}{" "}
+                    <strong>{t("benefits.2.text.high level of quality")}</strong>{" "}
+                    {t("benefits.2.text.of work")}.
+                  </BenefitThumbnail>
+                  <BenefitThumbnail
+                    heading={t("benefits.3.heading")}
+                    subheading={t("benefits.3.subheading")}
+                    image="./img/about/3.jpg"
+                    detailHeading={t("benefits.3.detailsHeading")}
+                    detailURL="#reviews"
+                  >
+                    {t("benefits.3.text.we believe that a cozy, beautiful and modern home repair means joy and happy life")}.
+                    <br />{" "}
+                    <strong>
+                      {t("benefits.3.text.your thankful reviews is our best reward")}!
+                    </strong>
+                  </BenefitThumbnail>
+                  <BenefitThumbnail
+                    inverted
+                    heading={t("benefits.4.heading")}
+                    image="./img/about/4.jpg"
+                  >
+                    {t("benefits.4.text.order a call and our operator will call you as soon as possible")}.
+                  </BenefitThumbnail>
+                </ul>
+              </div>
+            </div>
+
+            <div className="row justify-content-end mt-5">
+              <div className="col-auto">
+                <CallMeButton
+                  caption={t("benefits.orderACall")}
+                  size="md"
+                  className="shadow-sm text-uppercase"
+                />
+              </div>
             </div>
           </div>
-
-          <div className="row">
-            <div className="col-lg-12">
-              <ul className="timeline">
-                <BenefitThumbnail
-                  heading="Мы - профессионалы ремонта."
-                  subheading="Взгялине и убедитесь сами!"
-                  image="./img/about/1.jpg"
-                  detailHeading="Примеры наших работ"
-                  detailURL="#portfolio"
-                >
-                  Интериор - лидер рынке ремонта с{" "}
-                  <strong>более чем 10 летним</strong> стажем, и за это время мы
-                  выполнили <strong>свыше 300 работ</strong>. Мы знаем о ремонте
-                  всё и любим наше дело.
-                </BenefitThumbnail>
-                <BenefitThumbnail
-                  inverted
-                  heading="Мы гарантируем качество."
-                  subheading="И бесплатно устраним любые возникшие проблемы."
-                  image="./img/about/2.jpg"
-                >
-                  Наши клиенты могут рассчитывать на{" "}
-                  <strong>высокое качество</strong> выполненных работ.
-                </BenefitThumbnail>
-                <BenefitThumbnail
-                  heading="Наши клиенты нам благодарны."
-                  subheading="И мы это ценим."
-                  image="./img/about/3.jpg"
-                  detailHeading="Отзывы о нас"
-                  detailURL="#reviews"
-                >
-                  Мы верим, что уютный, красивый и современный ремонт в доме -
-                  залог радости и счастливой жизни.
-                  <br />{" "}
-                  <strong>
-                    Ваши благодарные отзывы для нас лучшая награда!
-                  </strong>
-                </BenefitThumbnail>
-                <BenefitThumbnail
-                  inverted
-                  heading="Мы ждём вас!"
-                  image="./img/about/4.jpg"
-                >
-                  Закажите звонок и наш оператор свяжется с вами в ближайшее
-                  время.
-                </BenefitThumbnail>
-              </ul>
-            </div>
-          </div>
-
-          <div className="row justify-content-end mt-5">
-            <div className="col-auto">
-              <CallMeButton
-                caption="Заказать звонок"
-                size="md"
-                className="shadow-sm text-uppercase"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-    );
+        </section>
+      );
+    }
   }
-}
+);
--- client/components/Portfolio.tsx
+++ client/components/Portfolio.tsx
@@ -1,163 +1,171 @@
 import React from "react";
 import PortfolioThumbnail from "./PortfolioThumbnail";
+import { WithTranslation } from "next-i18next";
+import { withTranslation } from "../../lib/i18n";
 
-export default class Portfolio extends React.Component {
-  render() {
-    return (
-      <section className="page-section" id="portfolio">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12 text-center">
-              <h2 className="section-heading text-uppercase">Наши работы</h2>
-              <h3 className="section-subheading text-muted">
-                С гордостью представляем вам некоторые из наших прошлых
-                проектов.
-              </h3>
+interface IPortfolioProps extends WithTranslation {}
+
+export default withTranslation()(
+  class Portfolio extends React.Component<IPortfolioProps> {
+    render() {
+      const { t } = this.props;
+      return (
+        <section className="page-section" id="portfolio">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12 text-center">
+                <h2 className="section-heading text-uppercase">
+                  {t("portfolio.heading")}
+                </h2>
+                <h3 className="section-subheading text-muted">
+                  {t("portfolio.subheading")}
+                </h3>
+              </div>
+            </div>
+
+            <div className="row">
+              <PortfolioThumbnail
+                heading={t("portfolio.1.heading")}
+                annotation={t("portfolio.1.annotation")}
+                imgSrc="img/portfolio/01-full.jpg"
+                date={t("portfolio.1.date")}
+                detail={t("portfolio.1.detail")}
+                client={t("portfolio.1.client")}
+                category={t("portfolio.1.category")}
+                slides={[
+                  {
+                    original: "img/portfolio/01-full.jpg",
+                    thumbnail: "img/portfolio/01-thumbnail.jpg"
+                  },
+                  {
+                    original: "img/portfolio/02-full.jpg",
+                    thumbnail: "img/portfolio/02-thumbnail.jpg"
+                  },
+                  {
+                    original: "img/portfolio/03-full.jpg",
+                    thumbnail: "img/portfolio/03-thumbnail.jpg"
+                  }
+                ]}
+              />
+              <PortfolioThumbnail
+                heading={t("portfolio.2.heading")}
+                annotation={t("portfolio.2.annotation")}
+                imgSrc="img/portfolio/02-full.jpg"
+                date={t("portfolio.2.date")}
+                detail={t("portfolio.2.detail")}
+                client={t("portfolio.2.client")}
+                category={t("portfolio.2.category")}
+                slides={[
+                  {
+                    original: "img/portfolio/02-full.jpg",
+                    thumbnail: "img/portfolio/02-thumbnail.jpg"
+                  },
+                  {
+                    original: "img/portfolio/03-full.jpg",
+                    thumbnail: "img/portfolio/03-thumbnail.jpg"
+                  },
+                  {
+                    original: "img/portfolio/04-full.jpg",
+                    thumbnail: "img/portfolio/04-thumbnail.jpg"
+                  }
+                ]}
+              />
+              <PortfolioThumbnail
+                heading={t("portfolio.3.heading")}
+                annotation={t("portfolio.3.annotation")}
+                imgSrc="img/portfolio/03-full.jpg"
+                date={t("portfolio.3.date")}
+                detail={t("portfolio.3.detail")}
+                client={t("portfolio.3.client")}
+                category={t("portfolio.3.category")}
+                slides={[
+                  {
+                    original: "img/portfolio/03-full.jpg",
+                    thumbnail: "img/portfolio/03-thumbnail.jpg"
+                  },
+                  {
+                    original: "img/portfolio/04-full.jpg",
+                    thumbnail: "img/portfolio/04-thumbnail.jpg"
+                  },
+                  {
+                    original: "img/portfolio/05-full.jpg",
+                    thumbnail: "img/portfolio/05-thumbnail.jpg"
+                  }
+                ]}
+              />
+              <PortfolioThumbnail
+                heading={t("portfolio.4.heading")}
+                annotation={t("portfolio.4.annotation")}
+                imgSrc="img/portfolio/07-full.jpg"
+                date={t("portfolio.4.date")}
+                detail={t("portfolio.4.detail")}
+                client={t("portfolio.4.client")}
+                category={t("portfolio.4.category")}
+                slides={[
+                  {
+                    original: "img/portfolio/04-full.jpg",
+                    thumbnail: "img/portfolio/04-thumbnail.jpg"
+                  },
+                  {
+                    original: "img/portfolio/05-full.jpg",
+                    thumbnail: "img/portfolio/05-thumbnail.jpg"
+                  },
+                  {
+                    original: "img/portfolio/06-full.jpg",
+                    thumbnail: "img/portfolio/06-thumbnail.jpg"
+                  }
+                ]}
+              />
+              <PortfolioThumbnail
+                heading={t("portfolio.5.heading")}
+                annotation={t("portfolio.5.annotation")}
+                imgSrc="img/portfolio/05-full.jpg"
+                date={t("portfolio.5.date")}
+                detail={t("portfolio.5.detail")}
+                client={t("portfolio.5.client")}
+                category={t("portfolio.5.category")}
+                slides={[
+                  {
+                    original: "img/portfolio/05-full.jpg",
+                    thumbnail: "img/portfolio/05-thumbnail.jpg"
+                  },
+                  {
+                    original: "img/portfolio/06-full.jpg",
+                    thumbnail: "img/portfolio/06-thumbnail.jpg"
+                  },
+                  {
+                    original: "img/portfolio/07-full.jpg",
+                    thumbnail: "img/portfolio/07-thumbnail.jpg"
+                  }
+                ]}
+              />
+              <PortfolioThumbnail
+                heading={t("portfolio.6.heading")}
+                annotation={t("portfolio.6.annotation")}
+                imgSrc="img/portfolio/06-full.jpg"
+                date={t("portfolio.6.date")}
+                detail={t("portfolio.6.detail")}
+                client={t("portfolio.6.client")}
+                category={t("portfolio.6.category")}
+                slides={[
+                  {
+                    original: "img/portfolio/06-full.jpg",
+                    thumbnail: "img/portfolio/06-thumbnail.jpg"
+                  },
+                  {
+                    original: "img/portfolio/07-full.jpg",
+                    thumbnail: "img/portfolio/07-thumbnail.jpg"
+                  },
+                  {
+                    original: "img/portfolio/08-full.jpg",
+                    thumbnail: "img/portfolio/08-thumbnail.jpg"
+                  }
+                ]}
+              />
             </div>
           </div>
-
-          <div className="row">
-            <PortfolioThumbnail
-              heading="Офис. Москва, Малая Пролетарская"
-              annotation="Ремонт помещений под нужды бизнеса в сфере услуг"
-              imgSrc="img/portfolio/01-full.jpg"
-              date="Август, 2017"
-              detail="Заказ был очень нестандартный, но мы считаем, что получилось отлично!"
-              client="Геннадий"
-              category="Ремонт офисов"
-              slides={[
-                {
-                  original: "img/portfolio/01-full.jpg",
-                  thumbnail: "img/portfolio/01-thumbnail.jpg"
-                },
-                {
-                  original: "img/portfolio/02-full.jpg",
-                  thumbnail: "img/portfolio/02-thumbnail.jpg"
-                },
-                {
-                  original: "img/portfolio/03-full.jpg",
-                  thumbnail: "img/portfolio/03-thumbnail.jpg"
-                }
-              ]}
-            />
-            <PortfolioThumbnail
-              heading="Офис. Москва, Проспект Октября"
-              annotation="Капитальный ремонт и обустройство помещений с нуля"
-              imgSrc="img/portfolio/02-full.jpg"
-              date="Август, 2017"
-              detail="Заказ был очень нестандартный, но мы считаем, что получилось отлично!"
-              client="Геннадий"
-              category="Ремонт офисов"
-              slides={[
-                {
-                  original: "img/portfolio/01-full.jpg",
-                  thumbnail: "img/portfolio/01-thumbnail.jpg"
-                },
-                {
-                  original: "img/portfolio/02-full.jpg",
-                  thumbnail: "img/portfolio/02-thumbnail.jpg"
-                },
-                {
-                  original: "img/portfolio/03-full.jpg",
-                  thumbnail: "img/portfolio/03-thumbnail.jpg"
-                }
-              ]}
-            />
-            <PortfolioThumbnail
-              heading="Детский сад. Москва, Громова 42"
-              annotation="Частный детский сад, капитальный ремонт детских комнат"
-              imgSrc="img/portfolio/03-full.jpg"
-              date="Август, 2017"
-              detail="Заказ был очень нестандартный, но мы считаем, что получилось отлично!"
-              client="Геннадий"
-              category="Ремонт офисов"
-              slides={[
-                {
-                  original: "img/portfolio/01-full.jpg",
-                  thumbnail: "img/portfolio/01-thumbnail.jpg"
-                },
-                {
-                  original: "img/portfolio/02-full.jpg",
-                  thumbnail: "img/portfolio/02-thumbnail.jpg"
-                },
-                {
-                  original: "img/portfolio/03-full.jpg",
-                  thumbnail: "img/portfolio/03-thumbnail.jpg"
-                }
-              ]}
-            />
-            <PortfolioThumbnail
-              heading="Кафе. Москва, Громова 17"
-              annotation="Дизайн и ремонт помещения для гостей"
-              imgSrc="img/portfolio/07-full.jpg"
-              date="Август, 2017"
-              detail="Заказ был очень нестандартный, но мы считаем, что получилось отлично!"
-              client="Геннадий"
-              category="Ремонт офисов"
-              slides={[
-                {
-                  original: "img/portfolio/01-full.jpg",
-                  thumbnail: "img/portfolio/01-thumbnail.jpg"
-                },
-                {
-                  original: "img/portfolio/02-full.jpg",
-                  thumbnail: "img/portfolio/02-thumbnail.jpg"
-                },
-                {
-                  original: "img/portfolio/03-full.jpg",
-                  thumbnail: "img/portfolio/03-thumbnail.jpg"
-                }
-              ]}
-            />
-            <PortfolioThumbnail
-              heading="Частный дом. Санкт-Петербург"
-              annotation="Ремонт помещений по проекту заказчика."
-              imgSrc="img/portfolio/05-full.jpg"
-              date="Август, 2017"
-              detail="Заказ был очень нестандартный, но мы считаем, что получилось отлично!"
-              client="Геннадий"
-              category="Ремонт офисов"
-              slides={[
-                {
-                  original: "img/portfolio/01-full.jpg",
-                  thumbnail: "img/portfolio/01-thumbnail.jpg"
-                },
-                {
-                  original: "img/portfolio/02-full.jpg",
-                  thumbnail: "img/portfolio/02-thumbnail.jpg"
-                },
-                {
-                  original: "img/portfolio/03-full.jpg",
-                  thumbnail: "img/portfolio/03-thumbnail.jpg"
-                }
-              ]}
-            />
-            <PortfolioThumbnail
-              heading="Детская комната"
-              annotation="Дизайн и обустройство детской комнаты."
-              imgSrc="img/portfolio/06-full.jpg"
-              date="Август, 2017"
-              detail="Заказ был очень нестандартный, но мы считаем, что получилось отлично!"
-              client="Геннадий"
-              category="Ремонт офисов"
-              slides={[
-                {
-                  original: "img/portfolio/01-full.jpg",
-                  thumbnail: "img/portfolio/01-thumbnail.jpg"
-                },
-                {
-                  original: "img/portfolio/02-full.jpg",
-                  thumbnail: "img/portfolio/02-thumbnail.jpg"
-                },
-                {
-                  original: "img/portfolio/03-full.jpg",
-                  thumbnail: "img/portfolio/03-thumbnail.jpg"
-                }
-              ]}
-            />
-          </div>
-        </div>
-      </section>
-    );
+        </section>
+      );
+    }
   }
-}
+);
